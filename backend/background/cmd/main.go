4a2155b8209ce6adb91ffc536ab1f46f7c8ff89e
atul161 <atulanand199898dbg@gmail.com>
2021-04-27 12:15:13 +0530

    Added

---
+++ backend/background/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	cadenceAdapter "alfred/background/adapters/cadence"
+	"alfred/background/config"
+	"alfred/background/workers/workflows"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting Worker..")
+	var cadenceAppConfig background.CadenceAppConfig
+	cadenceAppConfig.Setup()
+	var cadenceClient cadenceAdapter.CadenceAdapter
+	cadenceClient.Setup(&cadenceAppConfig.Cadence)
+    workflows.StartWorkers(&cadenceClient)
+	// The workers are supposed to be long running process that should not exit.
+	select {}
+}
+
