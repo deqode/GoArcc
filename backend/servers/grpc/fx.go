07c8358d5d9b3b6ce5d907a2107a6e4a565bd727
atul161 <atulanand199898dbg@gmail.com>
2021-04-28 19:41:54 +0530

    Cadence setup added

--- backend/servers/grpc/fx.go
+++ backend/servers/grpc/fx.go
@@ -2,7 +2,7 @@ package grpc
 
 import "go.uber.org/fx"
 
-//InitGrpcBeforeServingFx: Constructors present in the pipeline.
+// InitGrpcBeforeServingFx InitGrpcBeforeServingFx: Constructors present in the pipeline.
 var InitGrpcBeforeServingFx = fx.Options(
 	fx.Provide(
 		InitGrpcBeforeServing,
